31a15830f84631d79080ee437dc96b5adaf80fa0
Matthew Hyndman <myndman@gmail.com>
2020-09-27 15:36:09 -0400

    created a 2D scene as an alternative test - with a script to sort objects by z

---
+++ Assets/IsometricSort.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IsometricSort : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 pos = transform.localPosition;
+        // Set z = y, so that objects higher on the screen will be drawn in the back 
+        transform.localPosition = new Vector3(pos.x, pos.y, pos.y);
+    }
+}
