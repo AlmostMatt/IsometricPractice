78963ff0cc50ee2c824b5a31c17db01c26e321e9
Matthew Hyndman <myndman@gmail.com>
2020-10-01 00:28:26 -0400

    Add colliders to the scene and tweak some isometric velocity logic
    I realized that the isometric ratio for the image is close to 3:8, not 1:2
    However because of this wide ratio the character feels slow when moving vertically

---
+++ Assets/Scripts/IsometricUtilities.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class IsometricUtilities
+{
+    public const float X_PER_Y = 108f / 41f;
+    public const float Y_PER_X = 1f / X_PER_Y;
+
+    public static Vector3 ISO_RIGHT = new Vector3(X_PER_Y, 0, 0);
+    public static Vector3 ISO_UP = new Vector3(0, 1, 0);
+
+    public static Vector3 IsoToCartesian(Vector3 v)
+    {
+        return new Vector3(v.x, v.y / Y_PER_X, v.z);
+    }
+
+    public static Vector3 CartesianToIso(Vector3 v)
+    {
+        return new Vector3(v.x, v.y * Y_PER_X, v.z);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 movementDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        AccelToward(movementDir);
+    }
+
+    private void AccelToward(Vector2 cartesianMoveDir)
+    {
+        // Use cartesian coordinates to compute desired V, force required, etc.
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        Vector2 cartesianVel = IsometricUtilities.IsoToCartesian(rb.velocity);
+        Debug.Log(rb.velocity);
+        // If Movement dir has magnitude > 1, scale it down. Magnitude less than 1 is ok.
+        if (cartesianMoveDir.SqrMagnitude() > 1)
+        {
+            cartesianMoveDir.Normalize();
+        }
+        Vector2 desiredV = GetMaxSpeed() * cartesianMoveDir;
+        Vector2 deltaV = desiredV - cartesianVel;
+        float accelNeeded = deltaV.magnitude / Time.fixedDeltaTime;
+        Vector2 actualAccel = Mathf.Min(accelNeeded, GetAccel()) * deltaV.normalized;
+        // Convert back to isometric (screen) coordinates before applying the force
+        rb.AddForce(IsometricUtilities.CartesianToIso(actualAccel));
+    }
+
+    private float GetMaxSpeed()
+    {
+        return 6f;
+    }
+
+    private float GetAccel()
+    {
+        return 50f;
+    }
+}
